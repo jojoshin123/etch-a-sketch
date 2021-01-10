d86fad791bc7c3a8bb1857a54990870ec9d18cf9
Jonah Shin <jonahshin@Jonahs-MacBook-Pro.local>
2021-01-09 21:05:10 -0800

    Set up 32x32 grid

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".container");
 
 console.log(container);
 
+// Populate 32x32 grid
 for (let i = 0; i < 1024; i++) {
     const div = document.createElement('div');
     div.style.cssText = "border: 2px solid black;";
