1396d6fc04a03bfa2e1969a53b2749d2f4317341
Jonah Shin <jonahshin@Jonahs-MacBook-Pro.local>
2021-01-09 21:44:01 -0800

    Added basic drawing functionality

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 const container = document.querySelector(".container");
 
-console.log(container);
-
-// Populate 32x32 grid
-for (let i = 0; i < 1024; i++) {
+// Populate gridNum * gridNum grid
+let gridNum = 32
+for (let i = 0; i < gridNum * gridNum; i++) {
     const div = document.createElement('div');
-    div.style.cssText = "border: 2px solid black;";
+    div.style.cssText = "background-color: rgb(230, 230, 230);";
+    div.classList.add("cell");
     container.appendChild(div);
 }
+
+
+const cells = document.querySelectorAll(".cell");
+
+cells.forEach((cell) => {
+    cell.addEventListener("mousemove", () => {
+        cell.style.cssText = "background-color: black;";
+        console.log("poopman");
+    });
+});
